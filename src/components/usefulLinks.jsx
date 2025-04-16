7a2b65166cbd2dda3bbd09f76293440808b02e7c
David Hurrell <dhurrell@hotmail.co.uk>
2025-04-16 16:34:35 +0100

    Add useful link component. At present contains dummy info.

---
+++ src/components/usefulLinks.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styles from "./usefulLinks.module.css";
+function UsefulLinks() {
+  return (
+    <div className={styles["useful-links-container"]}>
+      <h2>Useful Links</h2>
+      <ul>
+        <li>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            MDN Web Docs - JavaScript
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.freecodecamp.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            freeCodeCamp
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.w3schools.com/js/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            W3Schools - JavaScript Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.w3schools.com/js/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            W3Schools - JavaScript Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.w3schools.com/js/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            W3Schools - JavaScript Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.w3schools.com/js/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            W3Schools - JavaScript Tutorial
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default UsefulLinks;
