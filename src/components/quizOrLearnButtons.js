0a86e3174fcf33883db554e6a8acf89b804457f0
David Hurrell <dhurrell@hotmail.co.uk>
2025-05-04 20:40:50 +0100

    Add component to display buttons that allow user to select between quiz and learn modes.

---
+++ src/components/quizOrLearnButtons.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./quizOrLearn.module.css";
+
+function QuizOrLearnButtons({ setIsLearn, setIsQuiz }) {
+  function handleLearnClick() {
+    setIsLearn(true);
+    setIsQuiz(false);
+  }
+  function handleQuizClick() {
+    setIsLearn(false);
+    setIsQuiz(true);
+  }
+  return (
+    <div className={styles["button-container"]}>
+      <button className={styles["button"]} onClick={handleQuizClick}>
+        Quiz
+      </button>
+      <button className={styles["button"]} onClick={handleLearnClick}>
+        Learn
+      </button>
+    </div>
+  );
+}
+
+export default QuizOrLearnButtons;
